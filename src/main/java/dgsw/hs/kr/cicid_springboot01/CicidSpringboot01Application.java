bbe49138aba7bc020ccf95b5ed84e41d949a4480
김건오 <gokim0207@dgsw.hs.kr>
2026-06-17 09:22:54 +0900

    first commit

---
+++ src/main/java/dgsw/hs/kr/cicid_springboot01/CicidSpringboot01Application.java
@@ -0,0 +1,13 @@
+package dgsw.hs.kr.cicid_springboot01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicidSpringboot01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CicidSpringboot01Application.class, args);
+    }
+
+}
---
+++ src/test/java/dgsw/hs/kr/cicid_springboot01/CicidSpringboot01ApplicationTests.java
@@ -0,0 +1,13 @@
+package dgsw.hs.kr.cicid_springboot01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicidSpringboot01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
