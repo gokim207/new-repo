86da8204aefb8445446cee0d0415ad314f868904
김건오 <gokim0207@dgsw.hs.kr>
2026-06-23 09:10:53 +0900

    controller update

---
+++ src/main/java/dgsw/hs/kr/cicid_springboot01/AppController.java
@@ -0,0 +1,13 @@
+package dgsw.hs.kr.cicid_springboot01;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AppController {
+
+    @GetMapping("")
+    public String hello() {
+        return "Hello World";
+    }
+}
